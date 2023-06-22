1fa69920bd751380669ed58e79a01f1b80d0ca66
raj-71 <roopaksharma1029@gmail.com>
2023-06-22 14:04:28 -0400

    final

--- chaincodes/chaincode-kv-node/node_modules/fabric-contract-api/lib/annotations/index.js
+++
@@ -1,8 +0,0 @@
-/*
- * Copyright IBM Corp. All Rights Reserved.
- *
- * SPDX-License-Identifier: Apache-2.0
- */
-
-'use strict';
-Object.assign(module.exports, require('./transaction'), require('./object'), require('./info'), require('./default'));
